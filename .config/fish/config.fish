6f4c4bcb5ab16c534cff904a9b4bc1d5c4787956
subzerock <ltphong112@gmail.com>
2023-02-24 11:16:28 -0800

    adding changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -184,11 +184,8 @@ alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l.='exa -a | egrep "^\."'
+alias ll "exa -l -g --icons"
+alias lla "ll -a"
 
 # pacman and yay
 alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
@@ -264,6 +261,7 @@ alias playmp3='deadbeef *.mp3'
 alias playavi='vlc *.avi'
 alias playmov='vlc *.mov'
 alias playmp4='vlc *.mp4'
+
 
 # youtube-dl
 alias yta-aac="youtube-dl --extract-audio --audio-format aac "
